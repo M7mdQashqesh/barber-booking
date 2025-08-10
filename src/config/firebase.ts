eac932cb67a7ce15731a3ea536094c12c337dd48
Mohammed Qashqesh <mo.qashqesh@gmail.com>
2025-08-10 13:40:46 +0300

    feat: implement login page structure and styles

---
+++ src/config/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCnZqpRuyt72FszNAOigorKm2sxeobmiJc",
+  authDomain: "barbershop-b18ca.firebaseapp.com",
+  projectId: "barbershop-b18ca",
+  storageBucket: "barbershop-b18ca.firebasestorage.app",
+  messagingSenderId: "1097661806282",
+  appId: "1:1097661806282:web:ae64f127cb253b9cc22633",
+  measurementId: "G-XYMW769EFK"
+};
+
+export const app = initializeApp(firebaseConfig);
