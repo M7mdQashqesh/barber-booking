d86b651853920fd53f8449c67c910431c97dd0c9
Mohammed Qashqesh <mo.qashqesh@gmail.com>
2025-08-14 18:32:01 +0300

    feat: start implement dashboard pages

--- src/scripts/appointments.ts
+++ src/scripts/appointments.ts
@@ -1,1 +1,8 @@
+const searchDiv = document.getElementById("search");
 
+searchDiv?.addEventListener("click", function () {
+  const searchInput = document.querySelector(
+    "input[type='search']"
+  ) as HTMLInputElement;
+  searchInput.focus();
+});
